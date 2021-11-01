f947d3764df673c9f5030ed12ad85d4d045f91c5
Julian Schubert <informatik.schubert@gmail.com>
2021-11-01 18:59:03 +0100

    Random attacking in cli tool

--- src/pokemon/cli/client.py
+++ src/pokemon/cli/client.py
@@ -12,10 +12,11 @@ from typing import List
 # TODO: Improve path input
 SHOWDOWN_PATH = "../pokemon-showdown/pokemon-showdown"
 
+# Second entry in the tuple: How many empty lines until all output is reccieved
 commands = [
-    '>start {"formatid":"gen7randombattle"}',
-    '>player p1 {"name":"Alice"}',
-    '>player p2 {"name":"Bob"}'
+    ('>start {"formatid":"gen7randombattle"}', 1),
+    ('>player p1 {"name":"Alice"}', 1),
+    ('>player p2 {"name":"Bob"}', 3)
 ]
 
 
@@ -29,33 +30,41 @@ def main():
     cli = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
 
     # Executing all commands to simulate a battle
-    for current_command in commands:
-        print(f"Current command:\"{current_command}\"")
+    for command in commands:
+        current_command = command[0]
+        blank_line_count = command[1]
 
-        output = _send_to_showdown(cli, current_command)
+        print(f"Current command:\"{current_command}\" ({blank_line_count})")
+
+        output = _send_to_showdown(cli, current_command, blank_line_count)
 
         print("\t{}".format('\n\t'.join([f"Line: {i} -> {output[i]}" for i in range(len(output))])))
         print("Flushed command!")
 
-        sys.exit(0)
+    
+    print("\n\nSuccessfully initialized battle, now, fight!\n\n")
+    
 
     # Hitting random
     for _ in range(20):
-        o1 = _send_to_showdown(cli, f">p1 move {random.randint(1, 4)}")
-        o2 = _send_to_showdown(cli, f">p2 move {random.randint(1, 4)}")
+        _send_to_showdown(cli, f">p1 move {random.randint(1, 4)}", 0)
+        battle_result = _send_to_showdown(cli, f">p2 move {random.randint(1, 4)}", 3)
 
-        print(f"{o1=}\n{o2=}")
 
-        _print_pokemon_info(o1[2])
-        _print_pokemon_info(o2[2])
+        # Printing info starting at the first move
+        battle_result_formatted = "\n\t".join(battle_result[10:])
+        print(f"\n\nBattle result:\n{battle_result_formatted}\n\n")
+
+        # Printing team info for both players
+        _print_pokemon_info(battle_result[2])
+        _print_pokemon_info(battle_result[6])
 
         p1 = 1  # Stores ID of active pokemon
         p2 = 1
 
-        break
 
-
-def _send_to_showdown(cli: subprocess.Popen, data: str, expected_sections=1) -> List[str]:
+def _send_to_showdown(cli: subprocess.Popen, data: str, expected_sections = 1) -> List[str]:
+    # TODO: Write Docstring
     # TODO: Ensure data is correct
 
     print(f"Sending Data to showdown: {data}")
@@ -64,24 +73,28 @@ def _send_to_showdown(cli: subprocess.Popen, data: str, expected_sections=1) -> 
         cli.stdin.write(f"{data}\n".encode())
         cli.stdin.flush()
 
-    # output = []
-    # while cli.poll() is None:
-    #     res = cli.stdout.readline().decode().strip()
-    #     print(f"Reccieved message from showdown: \"{res}\"")
-    #     if not res:
-    #         print(f"Exiting loop because res is empty")
-    #         break
-    #     output.append(res)
+
+    # No answer from showdown expected
+    if expected_sections == 0: return [""]
+
+    # Keeps track how many blank lines we have encountered
+    blank_counter = 0
 
     output = []
-    res = cli.stdout.readlines()
-
-    print(f"Showdown output: {res}")
+    while cli.poll() is None:
+        res = cli.stdout.readline().decode().strip()
+        #print(f"Reccieved message from showdown: \"{res}\"")
+        if not res:
+            blank_counter += 1
+            if blank_counter == expected_sections:
+                break
+        output.append(res)
 
     return output
 
 
 def _print_pokemon_info(info: str) -> None:
+
     request_part = json.loads(info.removeprefix("|request|"))
 
     # Getting info strings for all pokemon
