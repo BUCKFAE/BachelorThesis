aa0a23b6bc93021f37df7097644c860f789abdb7
Julian Schubert <informatik.schubert@gmail.com>
2021-12-21 13:19:26 +0100

    Short guide on how to setup the project

--- src/pokemon/config.py
+++ src/pokemon/config.py
@@ -1,10 +1,15 @@
 """Defines current configuration"""
 
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
 # Path where generated builds will be stored
-GENERATED_DATA_PATH = "/home/buckfae/Documents/BachelorThesis/src/data/generated"
+GENERATED_DATA_PATH = os.getenv("GENERATED_DATA_PATH")
 
 # Path to the damage calculator node application
-NODE_DAMAGE_CALCULATOR_PATH = "/home/buckfae/Documents/BachelorThesis/src/pokemon/bot/damage_calculator/lib"
+NODE_DAMAGE_CALCULATOR_PATH = os.getenv("NODE_DAMAGE_CALCULATOR_PATH")
 
 # How many turns to look ahead
 MATCHUP_MOVES_DEPTH = 1
