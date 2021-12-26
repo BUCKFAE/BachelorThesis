7cbc8632b39dcc1e791a64c64a21db80e7c02d03
Julian Schubert <informatik.schubert@gmail.com>
2021-12-27 00:02:33 +0100

    Challenging other players

---
+++ src/pokemon/bot/evaluation/challenge_player.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+import os
+import time
+
+from dotenv import load_dotenv
+from poke_env.player_configuration import PlayerConfiguration
+from poke_env.server_configuration import ShowdownServerConfiguration
+
+from src.pokemon.bot.RuleBasedPlayer import RuleBasedPlayer
+
+
+async def main():
+    load_dotenv()
+
+    showdown_user_name = os.getenv("SHOWDOWN_USER_NAME")
+    showdown_user_password = os.getenv("SHOWDOWN_USER_PASSWORD")
+    config = PlayerConfiguration(username=showdown_user_name, password=showdown_user_password)
+    player = RuleBasedPlayer(
+        battle_format="gen8randombattle",
+        player_configuration=config,
+        server_configuration=ShowdownServerConfiguration,
+        save_replays='src/data/replays',
+        max_concurrent_battles=1,
+        start_timer_on_battle_start=True)
+
+    await player.send_challenges("HerrDonner2", 1)
+
+    print(f'Stats: {player.n_won_battles} / {player.n_lost_battles}')
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
