70b23d5470e98538774d16542c67b322a4286a7d
Julian Schubert <informatik.schubert@gmail.com>
2022-01-05 12:17:47 +0100

    Fixing abilities with -

--- src/pokemon/data_handling/abilities/convert_ability.py
+++ src/pokemon/data_handling/abilities/convert_ability.py
@@ -12,7 +12,7 @@ class AbilityTranslator:
             with open(f'src/pokemon/data_handling/abilities/ability_list.txt') as f:
                 all_abilities = [a.strip() for a in f.readlines()]
                 for current_ability in all_abilities:
-                    self.ability_table[re.sub("[0-9 ]+", "", current_ability).lower()] = current_ability
+                    self.ability_table[re.sub("[0-9- ]+", "", current_ability).lower()] = current_ability
 
 
     def ability_to_calc_ability(self, ability: str) -> str:
